484bc71e2d79e9729d9f9f3362027123cb4f5329
Abir Alarean <abir.alarean27me@gmail.com>
2021-12-18 19:50:38 +0800

    Search bar styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import SearchBar from './Searchbar/SearchBar';
 
 function App() {
 
   return (
     <div className="App">
       <p>Hello World</p>
+      <SearchBar />
     </div>
   );
 }
