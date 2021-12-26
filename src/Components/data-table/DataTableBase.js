d0e3a6b201d628f524ad1478b3a028bedf601dcf
Abir Alarean <abir.alarean27me@gmail.com>
2021-12-26 15:39:53 +0800

    remove checkbox feature

--- src/Components/data-table/DataTableBase.js
+++ src/Components/data-table/DataTableBase.js
@@ -1,14 +1,10 @@
 import DataTable from 'react-data-table-component';
-import Checkbox from '@material-ui/core/Checkbox';
 
-const selectProps = { indeterminate: isIndeterminate => isIndeterminate };
 
 function DataTableBase(props) {
     return (
         <DataTable
             pagination
-            selectableRowsComponent={Checkbox}
-            selectableRowsComponentProps={selectProps}
             dense
             title
             {...props}
